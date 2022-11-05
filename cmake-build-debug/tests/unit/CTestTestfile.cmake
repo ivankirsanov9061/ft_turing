ad549d488f4c1eafc47aaa8bb95c986568f64466
Ivan Kirsanov <imora@student.42.fr>
2022-11-05 05:56:33 -0400

    Add preparation and delete unuseful directories

--- cmake-build-debug/tests/unit/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/ivan/CLionProjects/ft_turing/tests/unit
-# Build directory: /home/ivan/CLionProjects/ft_turing/cmake-build-debug/tests/unit
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
