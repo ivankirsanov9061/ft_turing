15c68038a52c98766ff9c2d77e11440630fdbb72
Ivan Kirsanov <imora@student.42.fr>
2022-11-06 11:19:42 +0300

    Write help message when argc < 3

--- turing/ParseArguments/ParseArguments.cpp
+++ turing/ParseArguments/ParseArguments.cpp
@@ -5,15 +5,10 @@
 
 struct SArguments ParseArguments(int argc, const char * const *argv, std::ostream &output_stream_for_information)
 {
-    if (IsHelpArgumentGiven(argc, argv))
+    if (IsHelpArgumentGiven(argc, argv) || argc < 3)
     {
         WriteHelpInformation(output_stream_for_information);
         std::exit(EXIT_SUCCESS);
-    }
-
-    if (argc < 3)
-    {
-        throw std::invalid_argument("Invalid number of arguments");
     }
 
     return {argv[1], argv[2]};
