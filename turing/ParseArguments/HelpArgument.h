87de60580b39596013b62c6e016d117e6a1f0388
Ivan Kirsanov <imora@student.42.fr>
2022-11-06 13:51:52 +0300

    Delete everything

--- turing/ParseArguments/HelpArgument.h
+++
@@ -1,4 +0,0 @@
-#pragma once
-
-bool IsHelpArgumentGiven(int argc, const char * const *argv);
-void WriteHelpInformation(std::ostream &output_stream_for_information);
--- turing/ParseArguments/SArguments.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include <string>
-
-struct SArguments
-{
-    std::string json_file_path;
-    std::string input_data_for_tape;
-};
