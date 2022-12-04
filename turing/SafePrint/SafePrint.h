6bde1fcf2c4135c879bd08bc443ecee345722106
Ivan Kirsanov <imora@student.42.fr>
2022-12-04 02:26:54 -0500

    Final version of code

--- turing/SafePrint/SafePrint.h
+++ turing/SafePrint/SafePrint.h
@@ -21,22 +21,3 @@ void Write(T &&...value)
 {
     (Write(value), ...);
 }
-
-class SafePrint
-{
-public:
-    void write(const char *str, size_t len);
-
-    template <typename T>
-    SafePrint &operator << (T &&value)
-    {
-        std::cout << value;
-
-        if (std::cout.exceptions() == std::ios_base::failbit)
-        {
-            std::cout << '\r';
-            std::cout << value;
-        }
-        return *this;
-    }
-};
