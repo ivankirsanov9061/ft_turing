a042fc3af0776199b5f9f7290cc917e6ff2532de
Ivan Kirsanov <imora@student.42.fr>
2023-02-03 10:07:12 -0500

    Error was implemented when argument too long

--- turing/ParseArguments/ParseArguments.cpp
+++ turing/ParseArguments/ParseArguments.cpp
@@ -1,7 +1,9 @@
 #include "ParseArguments.h"
 #include "HelpArgument.h"
+#include "SafePrint.h"
 
 #include <iostream>
+#include <cstring>
 
 struct SArguments ParseArguments(int argc, const char * const *argv)
 {
@@ -9,6 +11,12 @@ struct SArguments ParseArguments(int argc, const char * const *argv)
     {
         WriteHelpInformation();
         std::exit(EXIT_SUCCESS);
+    }
+
+    if (std::strlen(argv[2]) >= 21)
+    {
+        Write("Argument was too long\n");
+        std::exit(EXIT_FAILURE);
     }
 
     return {argv[1], argv[2]};
